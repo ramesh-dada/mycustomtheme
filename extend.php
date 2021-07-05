75254fec22d2763021faf1f4e3178a6dc2fd1cdd
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-05 11:45:43 +0530

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of sycho/flarum-asirem.
- *
- * Copyright (c) 2021 Sami Ilyes Mazouz.
- *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
- */
-
 namespace Afrux\Asirem;
 
 use Flarum\Extend;
