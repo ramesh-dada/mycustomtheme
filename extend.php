176e646584df8de064f1e3e56b741e18380b3d69
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-24 13:27:10 +0530

    Update extend.php

--- extend.php
+++ extend.php
@@ -6,8 +6,6 @@ use Flarum\Extend;
 use Flarum\Frontend\Document;
 
 return [
-    new \Afrux\ThemeBase\Extend\Init('ramesh-dada-customtheme'),
-
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less'),
