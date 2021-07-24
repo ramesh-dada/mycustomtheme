c4148e85ff98fad900d75b62b9dbfe6e4a16ca15
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-24 12:05:27 +0530

    Update extend.php

--- extend.php
+++ extend.php
@@ -14,6 +14,4 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less'),
-
-    new Extend\Locales(__DIR__.'/locale'),
 ];
