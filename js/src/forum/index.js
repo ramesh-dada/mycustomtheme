d481f19b7cd42615887eb9afe42818b9c00b4fc0
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-24 13:36:16 +0530

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,8 +1,7 @@
-import { extend, override } from 'flarum/common/extend';
+import { override } from 'flarum/common/extend';
 import TagsPage from 'flarum/tags/components/TagsPage';
 import AsiremTagsPage from './components/TagsPage';
-import { truncate } from 'flarum/utils/string';
 
-app.initializers.add('custom-tagspage', () => {
-  override(TagsPage.prototype, 'view', AsiremTagsPage.prototype.view);
+app.initializers.add('afrux-asirem', () => {
+ override(TagsPage.prototype, 'view', AsiremTagsPage.prototype.view);
 });
